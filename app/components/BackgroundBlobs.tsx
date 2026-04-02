33935fafb49ee492fc95bd12b626c20bb8d525a4
madewithbill <165756396+madewithbill@users.noreply.github.com>
2026-04-01 23:47:02 -0700

    add animation

--- app/components/BackgroundBlobs.tsx
+++ app/components/BackgroundBlobs.tsx
@@ -3,20 +3,20 @@ import styles from "@/app/index.module.css";
 export default function BackgroundBlobs() {
   return (
     <div className="absolute inset-0 z-0">
-      <div className="absolute inset-0 z-1 bg-[url(/noise.svg)] bg-repeat opacity-10 bg-blend-hard-light" />
+      <div className="absolute inset-0 z-1 bg-[url(/noise.svg)] bg-repeat opacity-5 bg-blend-hard-light" />
       <div className="absolute inset-0 blur-[150px]">
         {/* <div className="absolute inset-0"> */}
         <div
-          className={`${styles.animateBlob1} absolute right-114 bottom-0 z-4 h-100 w-82 bg-[#FE7B57]/75`}
+          className={`${styles.animateBlob1} absolute right-114 bottom-0 z-4 h-100 w-82 bg-[#FE7B57]/85`}
         />
         <div
-          className={`${styles.animateBlob2} absolute -right-32 -bottom-80 z-3 size-232 rounded-full bg-radial from-[#FF961F] from-40% to-[#FF961F]/11`}
+          className={`${styles.animateBlob2} absolute -right-32 -bottom-80 z-3 size-200 rounded-full bg-radial from-[#ff9317] from-40% to-[#ff9317]/20`}
         />
         <div
-          className={`${styles.animateBlob3} absolute -top-10 right-78 z-2 h-207 w-90 bg-white`}
+          className={`${styles.animateBlob3} absolute -top-10 right-78 z-2 h-100 w-90 bg-white`}
         />
         <div
-          className={`${styles.animateBlob4} absolute -top-138.5 left-55.75 z-1 size-232 rounded-full bg-radial from-[#0AB5D3] from-40% to-[#0AB5D3]/11`}
+          className={`${styles.animateBlob4} absolute -top-138.5 left-55.75 z-1 size-215 rounded-full bg-radial from-[#0AB5D3] from-40% to-[#0AB5D3]/20`}
         />
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import styles from "@/app/index.module.css";
+
 import Navbar from "./components/Navbar";
 import BackgroundBlobs from "./components/BackgroundBlobs";
 
@@ -7,37 +8,43 @@ export default function Home() {
     <>
       <Navbar />
       <section className="relative h-screen overflow-hidden">
-        <div className="relative z-1 m-auto flex h-full max-w-4xl flex-col items-center justify-center">
-          <div className="mb-14 flex h-8 items-center gap-2 rounded-full border border-white/20 bg-white/15 px-6 shadow-md">
+        <div className="relative z-1 m-auto flex h-full max-w-4xl flex-col items-center justify-center text-center font-medium">
+          <div
+            className={`${styles.animateIn} mb-14 flex h-8 items-center gap-2 rounded-full border border-white/20 bg-white/15 px-6 shadow-md`}
+          >
             <div className="size-2 rounded-full bg-[#49E83E]"></div>
-            <span className="text-sm leading-none">
+            <span className="text-sm leading-none font-normal">
               Version 2.3 now in beta!
             </span>
           </div>
-          <div className="flex flex-col items-center gap-16">
-            <div className="text-center font-medium text-shadow-[0_0px_10px_rgb(0_0_0/0.15)]">
-              <h1 className="mb-9 text-7xl leading-[110%]">
-                The last payment gateway you’ll ever need.
-              </h1>
-              <p>
-                Cut through the noise. Give your business fiscal clarity to
-                focus on growth.
-              </p>
-            </div>
-            <div className="flex items-center gap-4 font-medium">
-              <a
-                className="flex h-11 w-40.75 items-center justify-center rounded-lg bg-white px-3 py-2.5 leading-none text-[#060606]"
-                href=""
-              >
-                Get Started
-              </a>
-              <a
-                className="flex h-11 w-40.75 items-center justify-center"
-                href=""
-              >
-                Contact our Team
-              </a>
-            </div>
+
+          <h1
+            className={`${styles.animateIn} mb-9 text-7xl leading-[110%] text-shadow-[0_0px_10px_rgb(0_0_0/0.15)]`}
+          >
+            The last payment gateway you’ll ever need.
+          </h1>
+          <p
+            className={`${styles.animateIn} text-shadow-[0_0px_10px_rgb(0_0_0/0.15) text-lg] mb-15.5 font-normal`}
+          >
+            Cut through the noise. Give your business fiscal clarity to focus on
+            growth.
+          </p>
+
+          <div
+            className={`${styles.animateIn} flex items-center gap-4 font-medium`}
+          >
+            <a
+              className="flex h-11 w-40.75 items-center justify-center rounded-lg bg-white px-3 py-2.5 leading-none text-[#060606]"
+              href=""
+            >
+              Get Started
+            </a>
+            <a
+              className="flex h-11 w-40.75 items-center justify-center"
+              href=""
+            >
+              Contact our Team
+            </a>
           </div>
         </div>
         <BackgroundBlobs />
